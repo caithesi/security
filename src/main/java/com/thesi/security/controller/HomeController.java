b6fb8021c1a540c04f6c72c25ae140999932eef4
sict <sict@bap.jp>
2022-06-02 17:45:11 +0700

    init pj

---
+++ src/main/java/com/thesi/security/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.thesi.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @GetMapping
+  public String home() {
+    return "home";
+  }
+
+  @GetMapping("/user")
+  public String user() {
+    return "user";
+  }
+
+  @GetMapping("/admin")
+  public String admin() {
+    return "admin";
+  }
+}
