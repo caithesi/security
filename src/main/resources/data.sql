686fdac966dd3551c4a6e73eb43947f08424bc1d
sict <sict@bap.jp>
2022-06-03 10:19:22 +0700

    update code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,5 @@
-INSERT INTO users(username, password, enabled)
-values('user', 'user', 'true');
+INSERT INTO users(username, password, active, role)
+values('user', 'user', 'true', 'ROLE_USER');
 
-INSERT INTO users(username, password, enabled)
-values('admin', 'admin', 'true');
-
-INSERT INTO authorities(username, authority)
-values('user', 'ROLE_USER');
-
-INSERT INTO authorities(username, authority)
-values('admin', 'ROLE_ADMIN');
+INSERT INTO users(username, password, active, role)
+values('admin', 'admin', 'true', 'ROLE_ADMIN');
